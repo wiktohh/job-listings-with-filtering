d4230d58f95f784866a00d717520db594cf780de
wiktohh <wiktor.rzeznicki01@gmail.com>
2023-04-19 10:58:48 +0200

    remove tags

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import './styles/App.scss';
 import Main from './components/Main';
 
+import {ReactComponent as Remove} from "./images/icon-remove.svg"
+
 function App() {
 
   const [tags, setTags] = useState<string[]>([])
@@ -12,13 +14,35 @@ function App() {
     }
   }
 
+  const removeTag = (tag:string) => {
+    const tmp = [...tags]
+    const index = tmp.indexOf(tag)
+    if(index > -1){
+      tmp.splice(index,1)
+    }
+    setTags([...tmp])
+  }
+
+  const clearTags = () => {
+    setTags([])
+  }
   return (
     <div className="App">
       <div className="header"></div>
       {tags.length === 0 ? null : (
-        <div className="tags">{tags.map(tag => 
-            <div className="tag">{tag}<button className='removeBtn'>X</button></div>
-          )}</div>
+        <div className="tags">
+          <div className='tagWrapper'>
+            {tags.map(tag => (
+          <div className='tag'>
+            <p className='textTag'>{tag}</p>
+            <button onClick={() => removeTag(tag)} className='removeBtn'><Remove/></button>
+          </div>
+        )
+
+          )}
+          </div>
+          <button onClick={clearTags} className='clearBtn'>Clear</button>
+          </div>
       )}
       <Main handleTags={handleTags}/>
     </div>
